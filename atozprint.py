aa77e3bf0b63f3bd4877bcb984c764f0c5d1a126
San123567 <sanikasahu8@gmail.com>
2026-02-07 22:17:15 +0530

    Printing A to Z Using List Comprehension

---
+++ atozprint.py
@@ -0,0 +1,5 @@
+# Generates list of uppercase letters
+print("Alphabet A-Z:", " ".join([chr(i) for i in range(65, 91)]))
+
+# Generates list of lowercase letters
+print("Alphabet a-z:", " ".join([chr(i) for i in range(97, 123)]))
